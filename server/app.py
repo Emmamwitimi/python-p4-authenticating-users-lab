3cd1dc05886fc1eb7913439ceef0fbefece7d5ae
Emmaculate <emmamwitimi14@gmail.com>
2024-10-14 11:23:24 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -52,6 +52,48 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class loginUser(Resource):
+    def post(self):
+        data=request.get_json()
+        username= data.get('username')
+
+        user=User.query.filter_by(username=username).first()
+
+        #check if user exist
+        if user:
+            session['user_id'] = user.id
+
+            return make_response(jsonify({
+                'id': user.id,
+                'username': user.username
+            }), 200)
+        return {'error': 'User not found'}, 404
+api.add_resource(loginUser,'/login')
+
+
+class userLogout(Resource):
+    def delete(self):
+    
+        if 'user_id' in session:
+    
+            session.pop('user_id',None)
+
+            return {},204
+        
+api.add_resource(userLogout, '/logout')
+
+class check_session(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter_by(id=session['user_id']).first()
+            if user:
+                return make_response(jsonify(user.to_dict()), 200)
+        # Return 401 Unauthorized if no user_id in session or user not found
+        return {}, 401
+        
+api.add_resource(check_session,'/check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
